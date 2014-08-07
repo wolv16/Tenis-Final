e74407fb5b0411d0dbccb31bb984847fb2ffd01e
wolv16 <wolv16@hotmail.com>
2014-08-07 14:03:46 -0500

    Create Idioma.java
    Interface idioma

---
+++ src/Idioma.java
@@ -0,0 +1,12 @@
+public interface Idioma {
+    public String getLove();
+    public String get0();
+    public String get15();
+    public String get30();
+    public String get40();
+    public String getDeuce();
+    public String getAll();
+    public String getAdvantage();
+    public String getWin();
+    
+}
